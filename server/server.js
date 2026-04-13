741b9af7091537e9558bdcd00fe880fb0fb01c06
anuragjuyal2005 <anuragjuyal2005@gmail.com>
2026-04-14 00:21:35 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -47,9 +47,13 @@ app.use("/api/messages", messageRouter);
 
 // connect to MongoDB
 await connectDB();
-const PORT = process.env.PORT || 5000;
 
 // Start server
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+//export server for vercel
+export default server;
